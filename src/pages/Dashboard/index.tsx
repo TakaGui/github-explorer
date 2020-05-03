7d104841c2f98b565a82a888aadaa9ebb047e995
TakaGui <gui.yukio175@gmail.com>
2020-05-03 18:45:17 -0300

    add a new repository and use API the GitHub API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -6,33 +6,65 @@ import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
-  const [repositories, setRepositories] = useState([]);
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
 
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositóro" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositóro"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="https://github.com/TakaGui/workshopdev">
-          <img
-            src="https://avatars2.githubusercontent.com/u/19439567?s=460&u=7697609774df2598e67ed652b468d6530f400e25&v=4"
-            alt="Guilherme Takahashi"
-          />
+        {repositories.map((repository) => (
+          <a
+            key={repository.full_name}
+            href="https://github.com/TakaGui/workshopdev"
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>workshopdev</strong>
-            <p>Workshop Dev feito com a Rocketseat</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={35} />
-        </a>
+            <FiChevronRight size={35} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
