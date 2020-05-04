be8a29d994ad5ae0d0ddfe69a8935786842e92a1
TakaGui <gui.yukio175@gmail.com>
2020-05-03 21:00:01 -0300

    navigation with routing

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -78,9 +79,9 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a
+          <Link
             key={repository.full_name}
-            href="https://github.com/TakaGui/workshopdev"
+            to={`/repository/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
@@ -93,7 +94,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={35} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
